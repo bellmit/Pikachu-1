ccef83e4226b59f34b4dae65cd33cc2e1433d0a9
36654 <wuchunquan@longshine.com>
2021-04-21 11:09:50 +0800

    -

--- src/main/java/com/pikachu/framework/database/core/SQLInfo.java
+++ src/main/java/com/pikachu/framework/database/core/SQLInfo.java
@@ -17,6 +17,7 @@ import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Desc SQL信息，根据bean类、注解信息创建对应的sql语句
@@ -24,38 +25,52 @@ import java.util.Map;
  * @Author AD
  */
 public class SQLInfo<T> {
-    
+
     // 数据库表名
     private String tableName;
+
     // 创建语句
     private String createSQL;
+
     // 删除语句
     private String deleteSQL;
+
     // 查找语句
     private String retrieveSQL;
+
     // 查询条数语句
     private String countSQL;
+
     // 更新语句
     private String updateSQL;
+
     // 更新Bean对象语句
     private String updateBeanSQL;
+
     // 主键
     private String retrievePK;
+
     // 数据库类型
     private DatabaseType dbType;
+
     // 所对应的java类
     private Class<T> dataClass;
+
     // 缓存
     private boolean caching;
+
     // 缓存历史
     private boolean cachingHistory;
+
     // 主键数组
     private String[] primaryKeys = PikachuArrays.EMPTY_STRING;
+
     // 大写PK
     private List<String> PKList = new ArrayList<>();
+
     // 类里面的所有方法数据
     private MethodData methods;
-    
+
     /**
      * 构造方法
      *
@@ -64,7 +79,7 @@ public class SQLInfo<T> {
      * @param dbType    数据库类型
      */
     public SQLInfo(Class<T> dataClass, TableInfo tableInfo, DatabaseType dbType) {
-        
+
         String tableName;
         // 没有用注解
         if (tableInfo == null) {
@@ -80,7 +95,7 @@ public class SQLInfo<T> {
             this.caching = tableInfo.isCaching();
             this.cachingHistory = tableInfo.isCachingHistory();
         }
-        
+
         // 根据数据库类型修正表名
         if (dbType != DatabaseType.MYSQL) {
             if (dbType == DatabaseType.SQLSERVER) {
@@ -117,7 +132,7 @@ public class SQLInfo<T> {
         // 生成插入语句
         this.createSQL = "INSERT INTO " + tableName + " (" + PikachuConverts.toString(
                 columns) + ") VALUES (" +
-                         PikachuStrings.duplicate("?", ",", columns.size()) + ")";
+                PikachuStrings.duplicate("?", ",", columns.size()) + ")";
         // 根据有注解的类,生成根据主键更新和查找的语句：Update语句和Select语句
         StringBuffer sb = new StringBuffer();
         // 判断表信息有孝心
@@ -160,17 +175,15 @@ public class SQLInfo<T> {
             this.caching = false;
             this.cachingHistory = false;
         }
-        
+
     }
-    
+
     /**
      * 根据给定的(字段名&值)创建Bean对象(反射调用set方法)
      *
      * @param columns 表字段，即Bean对象属性
      * @param values  字段值
-     *
      * @return 当前bean类型反射创建的对象
-     *
      * @throws Exception 反射机制,可能会有反射异常
      */
     public T createBean(String[] columns, Object[] values) throws Exception {
@@ -197,12 +210,11 @@ public class SQLInfo<T> {
         }
         return null;
     }
-    
+
     /**
      * 生成insert语句：insert into table (xxx,xxx) values (?,?)
      *
      * @param t bean对象
-     *
      * @return SQLParams
      */
     public SQLParams getCreate(T t) {
@@ -211,12 +223,11 @@ public class SQLInfo<T> {
         return params == null ? null : new SQLParams(createSQL, params.getParams(),
                 params.getTypes());
     }
-    
+
     /**
      * 生成根据主键查找的select语句:select * from table where pk=?
      *
      * @param pksValue 主键字段值
-     *
      * @return SQLParams
      */
     public SQLParams getByPrimary(Object[] pksValue) {
@@ -227,12 +238,11 @@ public class SQLInfo<T> {
             return null;
         }
     }
-    
+
     /**
      * 生成select语句:select * from table where xxx=?
      *
      * @param wheres 查询条件
-     *
      * @return SQLParams
      */
     public SQLParams getRetrieve(Where[] wheres) {
@@ -240,14 +250,13 @@ public class SQLInfo<T> {
         return sql == null ? null : new SQLParams(retrieveSQL + sql.getSql(), sql.getParams(),
                 sql.getTypes());
     }
-    
+
     /**
      * 根据wheres、orders条件生成查找语句,并排序<br/>
      * select * from table where xxx=? order by xxx asc,xxx desc
      *
      * @param wheres 查询条件
      * @param orders 排序条件
-     *
      * @return SQLParams
      */
     public SQLParams getRetrieve(Where[] wheres, KeyValue[] orders) {
@@ -264,12 +273,11 @@ public class SQLInfo<T> {
         return new SQLParams(retrieveSQL + whereSQL + orderSQL, where.getParams(),
                 where.getTypes());
     }
-    
+
     /**
      * 生成查找数量语句:select count(*) c from table where xxx=?
      *
      * @param wheres 查询条件
-     *
      * @return SQLParams
      */
     public SQLParams getCount(Where[] wheres) {
@@ -277,12 +285,11 @@ public class SQLInfo<T> {
         return where == null ? null : new SQLParams(
                 this.countSQL + where.getSql(), where.getParams(), where.getTypes());
     }
-    
+
     /**
      * 生成根据主键获取数据的语句:select count(*) c from table where pk=?
      *
      * @param bean bean对象
-     *
      * @return SQLParams
      */
     public SQLParams getCountByPrimary(T bean) throws Exception {
@@ -291,13 +298,12 @@ public class SQLInfo<T> {
         return pk == null ? null : new SQLParams(countSQL + pk.getSql(),
                 pk.getParams(), pk.getTypes());
     }
-    
+
     /**
      * 生成update更新参数：update table set(xxx=?,xxx=?) where xxx=? and xxx=? or xxx=?
      *
      * @param updates 更新字段&值
      * @param wheres  更新条件
-     *
      * @return SQLParams
      */
     public SQLParams getUpdate(KeyValue[] updates, Where[] wheres) {
@@ -322,99 +328,111 @@ public class SQLInfo<T> {
         // update table set
         return new SQLParams(this.updateSQL + updateSql + whereSQL, params, types);
     }
-    
+
     /**
      * 生成更新语句：update table set(xxx=?,xxx=?) where pk1=? and pk2=?
      *
      * @param bean bean对象
-     *
      * @return SQLParams
      */
     public SQLParams getUpdateBean(T bean) {
         if (PikachuStrings.isNull(updateBeanSQL)) {
             return null;
         }
-        
+
         SQLParams sql = SQLHelper.getUpdateBeanParams(methods.getMethodsGet(), PKList, bean);
         return sql == null ? null : new SQLParams(this.updateBeanSQL, sql.getParams(),
                 sql.getTypes());
     }
-    
+
     /**
      * 生成删除语句:delete from table where xxx=?
      *
      * @param deletes 删除条件
-     *
      * @return SQLParams
      */
     public SQLParams getDelete(Where[] deletes) {
         SQLParams delete = this.getWhere(deletes);
         if (delete == null) return null;
-        
+
         String whereSQL = delete.getSql();
         return new SQLParams(this.deleteSQL + whereSQL, delete.getParams(),
                 delete.getTypes());
     }
-    
+
     public DatabaseType getType() {
         return this.dbType;
     }
-    
+
     public Class<T> getDataClass() {
         return this.dataClass;
     }
-    
+
     public boolean isCaching() {
         return this.caching;
     }
-    
+
     public boolean isCachingHistory() {
         return this.cachingHistory;
     }
-    
+
     public String[] getPrimaryKeys() {
         return this.primaryKeys;
     }
-    
-    public Object[] getPrimaryKeysValue(T t){
+
+    /**
+     * 获取对象的主键值
+     *
+     * @param t 对象
+     * @return
+     */
+    public Object[] getPrimaryKeysValue(T t) {
+        // 要求非空
+        Objects.requireNonNull(t);
+        // 获取主键字段
         String[] PKs = getPrimaryKeys();
-        Map<String, MethodInfo> methodsGetMap = methods.getMethodsGetMap();
-        Object[] values = new Object[PKs.length];
-        for (int i = 0; i < PKs.length; i++) {
-            MethodInfo methodInfo = methodsGetMap.get(PKs[i]);
-            try {
-                Object v = methodInfo.getMethod().invoke(t);
-                values[i]=v;
-            } catch (Exception e) {
-                e.printStackTrace();
+        // 判断是否存在主键
+        if (PKs != null && PKs.length > 0) {
+            // 获取bean对象对应的get方法信息
+            Map<String, MethodInfo> methodsGetMap = methods.getMethodsGetMap();
+            // 创建主键值数组
+            Object[] values = new Object[PKs.length];
+            for (int i = 0; i < PKs.length; i++) {
+                MethodInfo methodInfo = methodsGetMap.get(PKs[i]);
+                try {
+                    Object v = methodInfo.getMethod().invoke(t);
+                    values[i] = v;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            return values;
         }
-        return values;
-       
+        return PikachuArrays.EMPTY;
     }
-    
-    public String getTableName(){
+
+    public String getTableName() {
         return this.tableName;
     }
-    
+
     public SQLParams getWhere(Where[] wheres) {
         return SQLHelper.getWhereParams(methods.getMethodsGetMap(), wheres);
     }
-    
+
     public String getOrderSQL(KeyValue[] orders) {
         return SQLHelper.getOrderSQL(methods.getMethodsGetMap(), orders);
     }
-    
+
     public DaoBeanReader<T> getBeanReader() {
         return new DaoBeanReader<>(dataClass, methods.getMethodsSetMap());
     }
-    
+
     public DaoListReader<T> getListReader() {
         return new DaoListReader<>(dataClass, methods.getMethodsSetMap());
     }
-    
+
     public DaoPageReader<T> getPageReader(int pageSize) {
         return new DaoPageReader<>(dataClass, methods.getMethodsSetMap(), pageSize);
     }
-    
+
 }
